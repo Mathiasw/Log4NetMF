4bceedabc50aab8814e0ac962278257cddf05719
Mathias Wynne <github@mathiasw.com>
2013-11-30 16:29:08 +0000

    Added own (empty) UdpClient class.
    Signed-off-by: Mathias Wynne <github@mathiasw.com>

---
+++ Netmf-test2.0/NETMF Additions/UdpClient.cs
@@ -0,0 +1,65 @@
+using System;
+using Microsoft.SPOT;
+
+namespace System.Net.Sockets
+{
+    /// <summary>
+    /// hand rolled UdpClient class.
+    /// </summary>
+    public class UdpClient : IDisposable
+    {
+        public UdpClient();
+        public UdpClient(AddressFamily family);
+        public UdpClient(int port);
+        public UdpClient(int port, AddressFamily family);
+        public UdpClient(IPEndPoint localEP);
+        public UdpClient(string hostname, int port);
+        public Socket Client { get; set; }
+        protected bool Active { get; set; }
+        public int Available { get; }
+        public short Ttl { get; set; }
+        public bool DontFragment { get; set; }
+        public bool MulticastLoopback { get; set; }
+        public bool EnableBroadcast { get; set; }
+        public bool ExclusiveAddressUse { get; set; }
+
+        #region IDisposable Members
+
+        void IDisposable.Dispose();
+
+        #endregion
+
+        public void Close();
+        protected virtual void Dispose(bool disposing);
+        public void Connect(string hostname, int port);
+        public void Connect(IPAddress addr, int port);
+        public void Connect(IPEndPoint endPoint);
+        public int Send(byte[] dgram, int bytes, IPEndPoint endPoint);
+        public int Send(byte[] dgram, int bytes, string hostname, int port);
+        public int Send(byte[] dgram, int bytes);
+
+        [HostProtection(SecurityAction.LinkDemand, ExternalThreading = true)]
+        public IAsyncResult BeginSend(byte[] datagram, int bytes, IPEndPoint endPoint, AsyncCallback requestCallback, object state);
+
+        [HostProtection(SecurityAction.LinkDemand, ExternalThreading = true)]
+        public IAsyncResult BeginSend(byte[] datagram, int bytes, string hostname, int port, AsyncCallback requestCallback, object state);
+
+        [HostProtection(SecurityAction.LinkDemand, ExternalThreading = true)]
+        public IAsyncResult BeginSend(byte[] datagram, int bytes, AsyncCallback requestCallback, object state);
+
+        public int EndSend(IAsyncResult asyncResult);
+        public byte[] Receive(ref IPEndPoint remoteEP);
+
+        [HostProtection(SecurityAction.LinkDemand, ExternalThreading = true)]
+        public IAsyncResult BeginReceive(AsyncCallback requestCallback, object state);
+
+        public byte[] EndReceive(IAsyncResult asyncResult, ref IPEndPoint remoteEP);
+        public void JoinMulticastGroup(IPAddress multicastAddr);
+        public void JoinMulticastGroup(IPAddress multicastAddr, IPAddress localAddress);
+        public void JoinMulticastGroup(int ifindex, IPAddress multicastAddr);
+        public void JoinMulticastGroup(IPAddress multicastAddr, int timeToLive);
+        public void DropMulticastGroup(IPAddress multicastAddr);
+        public void DropMulticastGroup(IPAddress multicastAddr, int ifindex);
+    }
+}
+
